2744020809ad77ef2fe703949a8b154233a05fcd
GoldenStone02 <thegoldenstone02@gmail.com>
2022-06-20 01:59:00 +0800

    Added more tables

--- db/db_init.sql
+++ db/db_init.sql
@@ -63,7 +63,28 @@ CREATE TABLE promotion (
     FOREIGN KEY (flight_id) REFERENCES flight(flight_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- # Used for storing of airline logos
+-- # Flight Promo Many to Many table.
+CREATE TABLE flight_promotion (
+    flight_id INT NOT NULL,
+    promotion_id INT NOT NULL,
+    PRIMARY KEY (flight_id, promotion_id),
+    FOREIGN KEY (flight_id) REFERENCES flight(flight_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (promotion_id) REFERENCES promotion(promotion_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- # Review Table
+CREATE TABLE review (
+    review_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    flight_id INT NOT NULL,
+    review_text VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (flight_id) REFERENCES flight(flight_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- # Used for storing of airport logos
 CREATE TABLE image (
     image_id INT NOT NULL AUTO_INCREMENT,
     flight_id INT NOT NULL,
@@ -71,3 +92,25 @@ CREATE TABLE image (
     PRIMARY KEY (image_id),
     FOREIGN KEY (flight_id) REFERENCES flight(flight_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+-- .. Adding of dummy data
+-- # Add users
+INSERT INTO user (username, email, contact, password, role, profile_pic_url) VALUES ('admin', 'admin@sp_air.com', '12345678', 'password123', 'Admin', 'test')
+INSERT INTO user (username, email, contact, password, role, profile_pic_url) VALUES ('user', 'user@sp_air.com', '12345678', 'password123', 'Customer', 'test')
+
+-- # Add airports
+INSERT INTO airport (name, country, description) VALUES ('Changi Airport', 'Singapore', 'Main International Airport of Singapore')
+INSERT INTO airport (name, country, description) VALUES ('Penang International Airport', 'Malaysia', 'Main International Airport of the State of Penang')
+INSERT INTO airport (name, country, description) VALUES ('San Francisco International Airport', 'USA', 'International Airport located in San Francisco, California')
+INSERT INTO airport (name, country, description) VALUES ('Narita International Airport', 'Japan', 'International Airport located in Narita, Chiba, Tokyo')
+INSERT INTO airport (name, country, description) VALUES ('Sydney Kingsford Smith Airport', 'Sydney', 'International Airport located in Mascot, New South Wales, Australia')
+
+-- # Add flights
+
+-- # Add bookings
+
+-- # Add promotions
+
+-- # Add flight_promotions
+
+-- # Add images
