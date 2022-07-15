a03180a7cde77c14d50681beb5bb9a8b6c2500ba
GoldenStone02 <thegoldenstone02@gmail.com>
2022-07-15 16:55:06 +0800

    Frontend Backup

---
+++ public/js/carousel.js
@@ -0,0 +1,18 @@
+// This is used for the colour shift of the carousel
+$(document).ready(function(){
+	// Highlight bottom nav links
+	var clickEvent = false;
+	$("#myCarousel").on("click", ".nav a", function(){
+		clickEvent = true;
+		$(this).parent().siblings().removeClass("active");
+		$(this).parent().addClass("active");		
+	}).on("slid.bs.carousel", function(e){
+		if(!clickEvent){
+			itemIndex = $(e.relatedTarget).index();
+			targetNavItem = $(".nav li[data-bs-slide-to='" + itemIndex + "']");
+			$(".nav li").not(targetNavItem).removeClass("active");
+			targetNavItem.addClass("active");
+		}
+		clickEvent = false;
+	});
+});
---
+++ public/js/formSubmit.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Highlight bottom nav links
+    var clickEvent = false;
+    $("#myCarousel").on("click", function() {
+        clickEvent = true;
+    });
+})
